1f8fe2e82b7ee6cb007c09a7e7db0b69fdd53d05
VladiusVostokus <kovalenkov222@gmail.com>
2024-10-21 11:30:57 +0300

    Draw multiple points
    I used an array to store positions of points to draw all of them
    so user can see all points on the screen, not only the last.

--- task.js
+++ task.js
@@ -1,24 +1,21 @@
 'use strict';
 
 const vsSource = `#version 300 es
-uniform vec2 uPosition;
-//in vec3 aColor;
-out vec3 vColor;
+in vec2 aPosition;
 
 void main() {
-    gl_Position = vec4(uPosition, 0.0, 1.0);
+    gl_Position = vec4(aPosition, 0.0, 1.0);
     gl_PointSize = 5.0;
-    vColor = vec3(1.0, 0.0, 0.0);
 }`;
 
 const fsSource = `#version 300 es
 precision mediump float;
 
-in vec3 vColor;
+uniform vec3 uColor;
 out vec4 fragColor;
 
 void main() {
-    fragColor = vec4(vColor, 1.0);
+    fragColor = vec4(uColor, 1.0);
 }`;
 
 function main() {
@@ -54,7 +51,10 @@ function main() {
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.useProgram(program);
 
-    const uPosition = gl.getUniformLocation(program,'uPosition');
+    const uColor = gl.getUniformLocation(program,'uColor');
+    const aPosition = gl.getAttribLocation(program, 'aPosition');
+    const positions = [];
+    const buffer = gl.createBuffer();
 
     function clearCanvas() {
         gl.clearColor(0.5, 0.2, 0.6, 1.0);
@@ -62,6 +62,7 @@ function main() {
     }
 
     let mode = '';
+    let pointsCount = 0;
 
     const clearButton = document.getElementById('clear');
     const pointButton = document.getElementById('point');
@@ -87,11 +88,21 @@ function main() {
     });
 
     canvas.addEventListener('mousedown', (e) => {
+        pointsCount++;
         const kx = canvas.width / 2;
         const ky = canvas.height / 2;
         const x = (e.clientX -  kx) / kx;
         const y = -(e.clientY -  ky) / ky;
-        gl.uniform2f(uPosition, x, y);
-        gl.drawArrays(gl.POINTS, 0, 1);
+        positions.push(x);
+        positions.push(y);
+
+        const bufferData = new Float32Array(positions)
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW);
+        gl.vertexAttribPointer(aPosition, 2 , gl.FLOAT, false, 2 * 4, 0);
+
+        gl.enableVertexAttribArray(aPosition);
+        gl.uniform3f(uColor, 1.0, 0.0, 0.0);
+        gl.drawArrays(gl.POINTS, 0, pointsCount);
     });
 }
